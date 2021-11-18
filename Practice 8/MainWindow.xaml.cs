a40beae51a0084a2d64943c0267b175c83a419a8
KAT KAT <kass.kiss@yandex.ru>
2021-11-18 22:32:44 +0300

    Добавьте файлы проекта.

---
+++ Practice 8/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice_8
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Sum_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(int.Parse(firsttriadA.Text), int.Parse(secondtriadA.Text));
+            outputResult.Text = pair.Sum();
+        }
+
+        private void DIfference_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(int.Parse(firsttriadA.Text), int.Parse(secondtriadA.Text));
+            outputResult.Text = pair.Difference();
+        }
+
+        private void Product_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(int.Parse(firsttriadA.Text), int.Parse(secondtriadA.Text));
+            outputResult.Text = pair.Product();
+        }
+
+        private void Quotient_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(int.Parse(firsttriadA.Text), int.Parse(secondtriadA.Text));
+            outputResult.Text = pair.Quotient();
+        }
+
+        private void Compare_Click(object sender, RoutedEventArgs e)
+        {
+            Pair firstpair = new Pair(int.Parse(firsttriadA.Text), int.Parse(secondtriadA.Text));
+            Pair secondpair = new Pair(int.Parse(firsttriadB.Text), int.Parse(secondtriadB.Text));
+
+            int result = firstpair.CompareTo(secondpair);
+
+            if (result < 0) MessageBox.Show("Пара А меньше пары Б");
+            if (result > 0) MessageBox.Show("Пара А больше пары Б");
+            if (result == 0) MessageBox.Show("Пара А равна паре Б");
+        }
+
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Выполнил Гаврюшин К. А. ИСП-34.", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Practice 8/Pair.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_8
+{
+    class Pair : ISum, IProduct, IQuotient, IDifference, IComparable, ICloneable
+    {
+        public Pair(int first, int second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public int First { get; private set; }
+        public int Second { get; private set; }
+
+        public string Sum() => $"{First} + {Second} = {First + Second}";
+    
+        public string Difference() => $"{First} - {Second} = {First - Second}";
+
+        public string Product() => $"{First} * {Second} = {First * Second}";
+
+        public string Quotient() => $"{First} / {Second} = {Convert.ToDouble(First) / Convert.ToDouble(Second)}";
+
+        public int CompareTo(object firstpair)
+        {
+            Pair pair = (Pair)firstpair;
+            return First + pair.First - (Second + pair.Second);
+        }
+
+        public Pair ShallowClone()
+        {
+            return (Pair)this.MemberwiseClone();
+        }
+
+        public object Clone()
+        {
+            return new Pair(First, Second);
+        }
+    }
+}
